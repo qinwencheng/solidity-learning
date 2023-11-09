52f50eda6bac943ad502d539d5cc5be3df71b298
qinwencheng <157475034@qq.com>
2023-11-10 00:17:11 +0800

    feat(init): :tada: init project
    https://learnblockchain.cn/docs/hardhat/tutorial/

---
+++ scripts/deploy.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+  
+
+  const Token = await ethers.getContractFactory("Token");
+  const token = await Token.deploy();
+
+  console.log("Token address:", await token.getAddress());
+}
+
+main()
+.then(() => process.exit(0))
+.catch(error => {
+  console.error(error);
+  process.exit(1);
+});
