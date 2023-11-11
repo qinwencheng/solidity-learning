229f514bc82bd6ae212ae83d5c99f27989441ddf
qinwencheng <157475034@qq.com>
2023-11-12 00:04:41 +0800

    feat: example-5

---
+++ contracts/solidity-by-example/5.Constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+    address public my_var_address = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public my_var_uint = 123;
+}
